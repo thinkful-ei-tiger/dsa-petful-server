ec887ecf9aa0d77f0e8e1f73a8964c639beada16
yefenny <yefennychireno@Yefennys-MacBook-Pro.local>
2021-02-03 23:31:46 -0500

    add config.js

--- modules/app/app.js
+++ modules/app/app.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const NODE_ENV = require('./config');
 
 const app = express();
+const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
 
+app.use(morgan(morganOption));
+app.use(helmet());
 app.use(cors());
 
 app.use('/people', require('../people/people.router'));
 app.use('/pets', require('../pets/pets.router'));
+
+app.use(function errorHandler(error, req, res, next) {
+  let response;
+  if (NODE_ENV === 'production') {
+    response = { error: { message: 'server error' } };
+  } else {
+    console.error(error);
+    response = { message: error.message, error };
+  }
+  res.status(500).json(response);
+});
 
 module.exports = app;
