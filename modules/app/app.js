83aefab46239530b060b37a8ac6b43fe3627378c
yefenny <yefennychireno@Yefennys-MacBook-Pro.local>
2021-02-03 23:41:43 -0500

    fix error

--- modules/app/app.js
+++ modules/app/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
-const { NODE_ENV, CLIENT_ORIGIN } = require('./config');
+const { NODE_ENV, CLIENT_ORIGIN } = require('../config');
 
 const app = express();
 const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
