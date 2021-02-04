6b77aeb009a0ebeef25f64880e357ce036c71f3a
yefenny <yefennychireno@Yefennys-MacBook-Pro.local>
2021-02-04 00:00:13 -0500

    change client origin

--- modules/config.js
+++ modules/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  CLIENT_ORIGIN:
+    process.env.CLIENT_ORIGIN ||
+    'https://petful-client-template-tan.vercel.app',
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development'
 };
